d694df3469f18ee1603cf45c3f3377efa049011b
alanmaranto <alanarostegui@hotmail.com>
2020-01-01 12:12:10 -0600

    mostrando los datos

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 
+const initialState = {
+    posts: []
+}
 class Post extends Component {
+    constructor(props){
+        super(props)
+        this.state = initialState;
+    }
+    
   async componentDidMount() {
     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
     const data =  await res.json();
-    console.log(data)
-  }
+    this.setState({
+        posts: data
+    })
+}
   render() {
+      const { posts } = this.state;
+      console.log(posts)
     return (
       <div>
         <h1>Posts</h1>
+        {posts.map(post => {
+            return <div key={post.id}>
+                <h1>{post.title}</h1>
+                <span>{post.body}</span>
+            </div>
+        })}
       </div>
     );
   }
